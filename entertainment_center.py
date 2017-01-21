82bb495313cc7d83dd227aabc91b7d7909db2b7f
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-01-21 11:43:24 -0800

    initialize Movie class and create an instance for Toy Story

---
+++ entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "The story of a young boy and his toys",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg.",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self, title, storyline, poster_url, trailer_url):
+        self.title = title
+        self.storyline = storyline
+        self.poster_url = poster_url
+        self.trailer_url = trailer_url
