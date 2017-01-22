cbef78414ca865a028ad00b376b0a1ef63c41a60
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-01-21 17:50:19 -0800

    create movie instances for the webpage

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,31 @@ import media
 
 toy_story = media.Movie("Toy Story",
                         "The story of a young boy and his toys",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg.",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+baahubali = media.Movie("Baahubali",
+                        "The story of a man who runs after his past",
+                        "https://i.ytimg.com/vi/pcl1r2qTla8/maxresdefault.jpg",
+                        "https://www.youtube.com/watch?v=sOEg_YZQsTI")
+
+inception = media.Movie("Inception",
+                        "A con artist tries to create a dream within a dream within a dream!",
+                        "https://trailers.apple.com/trailers/wb/inception/images/poster-large.jpg",
+                        "https://www.youtube.com/watch?v=YoHD9XEInc0")
+
+shawshank_redemption = media.Movie("Shawshank Redemption",
+                                   "A prisonser escapes from a prison and fills hope in a friend in the process",
+                                   "http://ia.media-imdb.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_.jpg",
+                                   "https://www.youtube.com/watch?v=6hB3S9bIaco")
+
+oceans_eleven = media.Movie("Ocean's Eleven",
+                            "A group of eleven people plan and rob a casino",
+                            "https://images-na.ssl-images-amazon.com/images/I/51HNXQS7Q1L._SY300_.jpg",
+                            "https://www.youtube.com/watch?v=imm6OR605UI")
+
--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+import webbrowser
+
 class Movie():
     def __init__(self, title, storyline, poster_url, trailer_url):
         self.title = title
         self.storyline = storyline
         self.poster_url = poster_url
         self.trailer_url = trailer_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_url)
